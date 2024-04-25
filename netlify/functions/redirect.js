7b4c09f89cca8a911dc20e14b1aaf35971ef3da5
dhiaa <dhiaaJhamed@gmail.com>
2024-04-25 11:08:40 +0300

    add handler for /magazine route
    the institutions has a new magazine and wants a card for it seperately as well

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,10 +1,26 @@
-const CURRENT_DESTINATION_URL = "https://imam-zain-institution.carrd.co/";
+const INTITUTION_SOCIALS = "https://imam-zain-institution.carrd.co/";
+const MAGAZINE_SOCIALS = "https://imam-zain-institution.carrd.co/";
 
 exports.handler = async (event, context) => {
     try {
         // Validate HTTP method
         if (event.httpMethod !== 'GET') {
             return { statusCode: 405, body: 'Method Not Allowed' };
+        }
+
+        // Check if request path is for /magazine
+        if (event.path === '/magazine') {
+            return {
+                statusCode: 302,
+                headers: {
+                    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+                    'X-Content-Type-Options': 'nosniff',
+                    'X-Frame-Options': 'DENY',
+                    'X-XSS-Protection': '1; mode=block',
+                    'Referrer-Policy': 'strict-origin-when-cross-origin',
+                    Location: MAGAZINE_SOCIALS
+                }
+            };
         }
 
         // Return the redirection response with security headers
@@ -16,7 +32,7 @@ exports.handler = async (event, context) => {
                 'X-Frame-Options': 'DENY',
                 'X-XSS-Protection': '1; mode=block',
                 'Referrer-Policy': 'strict-origin-when-cross-origin',
-                Location: CURRENT_DESTINATION_URL
+                Location: INTITUTION_SOCIALS
             }
         };
     } catch (error) {

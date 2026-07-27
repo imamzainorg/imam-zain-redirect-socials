e36987cf73f79904d2e1e49d1cc98970a3bc9657
aboturab <dhiaaJhamed@gmail.com>
2026-07-27 08:24:02 +0300

    Refactor redirect function to use LOCATION constant
    Updated redirection logic to use LOCATION constant for URL.

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,33 +1,11 @@
-const INTITUTION_SOCIALS = "https://imamzain.org/links";
-const MAGAZINE_SOCIALS = "https://imam-zain-institution-magazine.carrd.co/";
-
-const NEW_MERGED = "https://imamzainorg.carrd.co/"
+const LOCATION = "https://imamzain.org/links"
 
 exports.handler = async (event, context) => {
     try {
-        // Validate HTTP method
         if (event.httpMethod !== 'GET') {
             return { statusCode: 405, body: 'Method Not Allowed' };
         }
 
-        console.log(event.path);
-        // Check if request path is for /magazine
-        if (event.path === '/.netlify/functions/redirect/magazine') {
-            return {
-                statusCode: 302,
-                headers: {
-                    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-                    'X-Content-Type-Options': 'nosniff',
-                    'X-Frame-Options': 'DENY',
-                    'X-XSS-Protection': '1; mode=block',
-                    'Referrer-Policy': 'strict-origin-when-cross-origin',
-                    // Location: MAGAZINE_SOCIALS
-                    Location: NEW_MERGED
-                }
-            };
-        }
-
-        // Return the redirection response with security headers
         return {
             statusCode: 302,
             headers: {
@@ -36,8 +14,7 @@ exports.handler = async (event, context) => {
                 'X-Frame-Options': 'DENY',
                 'X-XSS-Protection': '1; mode=block',
                 'Referrer-Policy': 'strict-origin-when-cross-origin',
-                // Location: INTITUTION_SOCIALS
-                Location: NEW_MERGED
+                Location: LOCATION
             }
         };
     } catch (error) {
